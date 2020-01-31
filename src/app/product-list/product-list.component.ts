8dbeb540e42e60b6a967363785ebe15a269eeaff
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-01-31 18:40:13 +1100

    Array filter added and some code styling

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IProduct } from './product';
 
 @Component({
   selector: 'app-product-list',
@@ -10,8 +11,12 @@ export class ProductListComponent implements OnInit {
   pageTitle: string = "Products list";
   imageWidth: number = 50;
   imageMargin: number = 2;
-  listFilter: string = "cart";
+
   showImage: boolean = false;
+  filteredProducts: IProduct[] = [];
+
+  _listFilter: string = '';
+
   products: any[] = [{
     "productId": 1,
     "productName": "Leaf Rake",
@@ -60,12 +65,29 @@ export class ProductListComponent implements OnInit {
   }
   ];
 
-  constructor() { }
+  constructor() { 
+    this.filteredProducts = this.products;
+    this.listFilter='cart';
+  }
 
   ngOnInit() {
   }
 
   toggleImage(): void {
     this.showImage = !this.showImage;
+  }
+  
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+  }
+
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.products.filter((product: IProduct) =>
+      product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
   }
 }
