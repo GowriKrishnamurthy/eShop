8d716af8e05288451c5ef7a52ee5442094ef7f74
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-01-31 19:56:52 +1100

    cleanup: folder renamed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductListComponent } from './product-list/product-list.component';
+import { ProductListComponent } from './products/product-list.component';
 import { ConvertToSpacesPipe } from './common/convert-to-spaces.pipe';
 import { StarComponent } from './common/star/star.component';
 
