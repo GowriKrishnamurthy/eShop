10ee2f1eeed0b765afe199c683f1e7c056039f41
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-02-01 20:51:42 +1100

    new component for product detail and some refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ProductListComponent } from './products/product-list.component';
+import { AppComponent } from './app.component'; 
 import { ConvertToSpacesPipe } from './common/convert-to-spaces.pipe';
 import { StarComponent } from './common/star/star.component';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component';
+import { ProductListComponent } from './products/product-list/product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductListComponent,
     ConvertToSpacesPipe,
-    StarComponent
+    StarComponent,
+    ProductDetailComponent
   ],
   imports: [
     FormsModule,
