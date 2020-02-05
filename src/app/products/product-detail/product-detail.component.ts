835fd9365e575bcc4d153a71779a9e47a207d9ee
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-02-05 22:48:02 +1100

    passing params to route. WIP

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from '../product';
+import { ActivatedRoute } from '@angular/router';
+import { ProductService } from '../product.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +11,11 @@ export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: IProduct | undefined;
 
-  constructor() { }
+  constructor(private route:ActivatedRoute, private productService : ProductService) {
+   }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `:${id}`;
   }
-
 }
