eca562fcff4f614b4b9363e2af42ff3605877b80
GowriKrishnamurthy <35420783+GowriKrishnamurthy@users.noreply.github.com>
2020-02-07 20:12:36 +1100

    Feature module and Shared module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component'; 
-import { ConvertToSpacesPipe } from './common/convert-to-spaces.pipe';
-import { StarComponent } from './common/star/star.component';
-import { ProductDetailComponent } from './products/product-detail/product-detail.component';
-import { ProductListComponent } from './products/product-list/product-list.component';
-import { WelcomeComponent } from './welcome/welcome.component';
 import { RouterModule } from '@angular/router';
-import { ProductDetailGuard } from './products/product-detail.guard';
+import { ProductModule } from './products/product.module';
+import { AppRoutingModule } from './app-routing.module';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductListComponent,
-    ConvertToSpacesPipe,
-    StarComponent,
-    ProductDetailComponent,
     WelcomeComponent
   ],
   imports: [
-    FormsModule,
     BrowserModule,
-    AppRoutingModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      { path:'products', component:ProductListComponent},
-      { path:'products/:id', canActivate: [ProductDetailGuard], component:ProductDetailComponent},
-      { path:'welcome', component:WelcomeComponent},
-      { path:'', redirectTo:'welcome', pathMatch:'full'},
-      { path:'**', redirectTo:'welcome', pathMatch:'full'},
-  ])
+    ProductModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
